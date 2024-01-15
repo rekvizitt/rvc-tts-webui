1fd7f309063d76f748fe3c61d8178993da98615f
rekvizitt <chekaviyvlad@gmail.com>
2024-01-15 14:02:32 +0300

    Updated voice_api.py

--- voice_api.py
+++ voice_api.py
@@ -1,28 +1,6 @@
 import sys
 from gradio_client import Client
 
-from pydrive.auth import GoogleAuth
-from pydrive.drive import GoogleDrive
-import os
-
-gauth = GoogleAuth()
-
-# Try to load saved client credentials
-gauth.LoadCredentialsFile("mycreds.txt")
-if gauth.credentials is None:
-    # Authenticate if they're not there
-    gauth.LocalWebserverAuth()
-elif gauth.access_token_expired:
-    # Refresh them if expired
-    gauth.Refresh()
-else:
-    # Initialize the saved creds
-    gauth.Authorize()
-# Save the current credentials to a file
-gauth.SaveCredentialsFile("mycreds.txt")
-
-drive = GoogleDrive(gauth)
-
 args = sys.argv
 
 client_link = args[1]
