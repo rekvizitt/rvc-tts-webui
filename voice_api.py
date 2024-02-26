c15d2b6af70bd15028c97b5be04773423b5c65d4
rekvizitt <chekaviyvlad@gmail.com>
2024-02-26 14:05:49 +0300

    Small fix

--- voice_api.py
+++ voice_api.py
@@ -1,6 +1,7 @@
 import sys
 from gradio_client import Client
 import codecs
+import os
 
 sys.stdout = codecs.getwriter("utf-8")(sys.stdout.detach())
 
@@ -9,12 +10,12 @@ args = sys.argv
 client_link = args[1]
 model_name = args[2]
 user_input = args[3]
-
+file_name = args[4]
 
 client = Client(f"{client_link}")
 result = client.predict(
 	model_name,
-	20,
+	10,
 	user_input,
 	"ru-RU-DmitryNeural-Male",
 	2,
@@ -24,4 +25,7 @@ result = client.predict(
 	fn_index=0
 )
 
-print(result[2])
+with open(os.path.join("C:\\Users\\Vladislav\\AppData\\Roaming\\Godot\\app_userdata\\streamers_ai", file_name), "w") as file:
+    file.write(result[2])
+
+sys.exit(0)
